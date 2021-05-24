fdde658dcd92d3f3ac053e369b86613ef0fb4323
Bobby T <bob.tarino@gmail.com>
2021-05-24 15:18:53 -0500

    fixed test values

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -32,7 +32,7 @@ test("Can get name via getName()", () => {
 test("Can get id via getId()", () => {
   const testValue = 100;
   const e = new Employee("Andre", "test@test.com", testValue);
-  expect(e.getId()).toEqual(expect.any(Number));
+  expect(e.getId()).toBe(testValue);
 });
 
 test("Can get email via getEmail()", () => {
